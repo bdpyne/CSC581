3dc18662fba253e3165c11b06e5744bbf1831427
Bill Pyne <bdpyne@gmail.com>
2016-04-07 18:45:10 -0400

    Barfing when adding to counts

--- Assignment6/prog.r
+++ Assignment6/prog.r
@@ -7,13 +7,7 @@ run <- function() {
 	df    <- as.data.frame(iris)
 	rec   <- as.data.frame(df[137,])
 
-	print(sprintf("Label to find: %s", rec$Species))
-
-#	class <- classify(df, rec)
-#	print(class)
-
-	class <- my.test(df, rec)
-	print(sprintf("Label found is: %s", class))
+	class <- classify(df, rec)
 }
 
 
@@ -36,123 +30,13 @@ get.row.num <- function(ds, rec)  {
 	return(idx)
 }
 
-###############################################################################
-#
-###############################################################################
-classify <- function(df, pnt) {
 
-	# Convert for convenience
-	rec <- as.data.frame(pnt)	
+classify <- function(df, rec) {
 
-	# Get the point from the data frame - force it to be a vector
-	rec.label <- rec$Species
-
-	# Initialize just to let the interpreter know the data type
-	class <- ""
-
-	# Get the unique labels from the dependent column
-	labels <- unique(df[,ncol(df)])
-
-	# How many labels?
-	labels.cnt <- length(labels)
-
-	# Use the vector allocator to create a vector of the same length as the 
-	# number of labels
-	# Doing so also initializes the points to 0
-	cnt <- integer(labels.cnt)
-
-	p.max <- labels.cnt - 1
-
-	for (p in 1:p.max) {
-		
-		print(p)
-
-		q.min <- p + 1
-
-		for (q in q.min:labels.cnt) {
-
-			print(q)
-
-			# Get the label values 
-			l1 <- labels[p]
-			l2 <- labels[q]
-
-			# Get a new frame that is the union of # the rows matching 
-			# l1 and l2
-			ds <- df[ df[,ncol(df)] == l1 | df[,ncol(df)] == l2 ,]
-
-			# Create a duplicate of the ds data frame and add a row number
-			ds.with.index    <- data.frame(ds)
-            ds.with.index$Id <- seq.int(nrow(ds.with.index))
-
-
-            # Get row number of the rec in the data set.
-			# If none exists, then the value is 0.
-			row.num <- get.row.num(ds.with.index, rec)
-
-
-			print(row.num)	
-			print(is.null(row.num))
-			print(is.integer(row.num))
-
-			# Is in this pair of sets
-			if (row.num > 0) {
-				
-
-
-			###########################################################
-			# Train the data using svm() in this section
-			###########################################################
-			model <- svm(Species~., data=ds, type="C-classification", cost=1, kernel="linear")
-
-			###########################################################
-			# Find out how it did.
-			###########################################################
-			predict <- as.data.frame(fitted(model))
-
-			# ******  Need to find rec in the prediction set ******
-
-			predict.label <- predict[row.num,1]
-
-#			print(predict)
-
-			# *****************************************************
-
-
-		    if (predict.label == rec.label) {
-				cnt[p] <- cnt[p] + 1
-			}
-			else {
-				cnt[q] <- cnt[q] + 1
-			}
-
-			print(cnt)
-
-			###########################################################
-			###########################################################
-			}
-		}
-
-
-	}
-
-	# Get the maximum label count
-	max.cnt <- max(cnt)
-
-	# Now its index
-	max.cnt.idx <- match(max.cnt, cnt)
-
-	# Now get the label it's attached to
-	class <- labels[max.cnt.idx]
-
-
-	return(class)
-}
-
-my.test <- function(dfIn, rec) {
-
-	# Get the point from the data frame - force it to be a vector
+	# Get the point from the data frame
 	rec.label <- rec$Species[1]
+
+	print(sprintf("Label to find is: %s", rec.label))
 
 	# Initialize just to let the interpreter know the data type
 	class <- ""
@@ -160,52 +44,57 @@ my.test <- function(dfIn, rec) {
 	# Get the unique labels from the dependent column
 	labels <- unique(df$Species)
 
-#	print(labels)
 
 	# How many labels?
 	labels.cnt <- length(labels)
 
-	# Use the vector allocator to create a vector of the same length as the 
-	# number of labels
-	# Doing so also initializes the points to 0
-	cnt <- integer(labels.cnt)
+	# Allocate a 3 column data frame to hold the totals
+	cnt <- data.frame()
+	cnt[1,1] <- 0
+	cnt[1,2] <- 0
+	cnt[1,3] <- 0
+	colnames(cnt) <- c(labels[1],labels[2],labels[3])
+
+#	septosis.cnt   <- 0
+#	versicolor.cnt <- 0
+#	virginica.cnt  <- 0
 
 	# Get the upper index
 	p.max <- labels.cnt - 1
 
 	for (p in 1:p.max) {
-		
-#		cat("p: ", p, "\n")
 
-		l1    <- labels[p]
-#		print(l1)
+		p.label    <- labels[p]
+
+		print(sprintf("Debug 1"))
 
 		# Get the lower index
 		q.min <- p + 1
 
+		print(sprintf("Debug 2"))
+
 		for (q in q.min:labels.cnt) {
-#			cat("q: ", q, "\n")
 
 			# Get the label values 
-			l2 <- labels[q]
-			
-#			print(l2)
+			q.label <- labels[q]
 
 			# Get a new frame that is the union of # the rows matching 
-			# l1 and l2
-			ds <- df[ df[,ncol(df)] == l1 | df[,ncol(df)] == l2 ,]
+			# p.label and q.label
+			ds <- df[ df[,ncol(df)] == p.label | df[,ncol(df)] == q.label ,]
 
 			# Create a duplicate of the ds data frame and add a row number
 			ds.with.index    <- data.frame(ds)
             ds.with.index$Id <- seq.int(nrow(ds.with.index))
 
-#			print(ds.with.index)
+			print("Debug 3")
 
             # Get row number of the rec in the data set.
 			# If none exists, then the value is 0.
 			row.num <- get.row.num(ds.with.index, rec)
 
-#			print(row.num)
+			print(sprintf("Row num is: %d", row.num))
+
+			print("Debug 4")
 
 			# Is in this pair of sets
 			if (row.num > 0) {
@@ -224,15 +113,27 @@ my.test <- function(dfIn, rec) {
 			    # ******  Need to find rec in the prediction set ******
 			    predict.label <- predict[row.num,1]
 
+				print("Debug 5")
+
 		        if (predict.label == rec.label) {
-				    cnt[p] <- cnt[p] + 1
+					tot <- cnt[1,p.label]
+					tot <- tot + 1
+				    cnt[1,p.label] <- tot 
+					print(sprintf("p count is %d", cnt[1,p.label]))
 			    }
 			    else {
-				    cnt[q] <- cnt[q] + 1
+					tot <- cnt[1,q.label]
+					tot <- tot + 1
+				    cnt[1,q.label] <- tot 
+					print(sprintf("q count is %d", cnt[1,q.label]))
 			    }
+				print("Debug 6")
             }
 		}
 	}
+
+	
+	print(sprintf("%s: %d, %s: %d, %s: %d", labels[1], cnt[1,1], labels[2], cnt[1,2], labels[3], cnt[1,3]))
 
 	# Get the maximum label count
 	max.cnt <- max(cnt)
@@ -243,6 +144,7 @@ my.test <- function(dfIn, rec) {
 	# Now get the label it's attached to
 	class <- labels[max.cnt.idx]
 
+	print(sprintf("Label found is: %s", class))
 
 	return(class)
 }
